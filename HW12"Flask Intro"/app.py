9c0f63436c0261f42d28fa38187c60e7dd512816
inna <inna.shturnieva@gmail.com>
2022-06-08 20:48:33 +0300

    Simple calc app/ HW12 done

--- HW12"Flask Intro"/app.py
+++ HW12"Flask Intro"/app.py
@@ -1,22 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def welcome():
-    return 'Welcom to calculator app!'
+    return 'Welcome to calculator app!'
 
 
-@app.route('/calc')
+@app.route('/calc/')
 def calculator():
-    return render_template('calc.html')
+    return 'Simple Calculator App'
 
 
-@app.route('/calc/<int:x>/<int:y>')
-def calc(x,y):
-    return render_template('calc.html', x=x, y=y, div=x/y)
+@app.route('/calc/<int:x>/<int:y>/<string:operation>')
+def calc(x, y, operation):
+    if operation == "sum":
+        return render_template('calc.html', x=x, y=y, operation="+", result=x + y)
+    elif operation == "dif":
+        return render_template('calc.html', x=x, y=y, operation="-", result=x - y)
+    elif operation == "mult":
+        return render_template('calc.html', x=x, y=y, operation="*", result=x * y)
+    elif operation == "div":
+        return render_template('calc.html', x=x, y=y, operation="/", result=x / y)
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
